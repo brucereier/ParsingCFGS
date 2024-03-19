b75b78e885889ebe56d2c52669dffb32616225fb
brucereier <134559712+brucereier@users.noreply.github.com>
2024-03-19 16:52:24 -0400

    Update Recursive_descent.py

--- Recursive_descent.py
+++ Recursive_descent.py
@@ -65,7 +65,55 @@ def improved_match(w: str, start: int, end: int, non_terminal: str) -> bool:
             
     return False
 
-w = "bbb"
-print(improved_match(w, 1, len(w), "S"))
+#w = "bbb"
+#print(improved_match(w, 1, len(w), "S"))
+
+look_ahead = None
+input_stream = None
+parsing_success = True
+
+def match2(expected):
+    print(expected)
+    global look_ahead, parsing_success
+    if look_ahead == expected:
+        read_next_char()
+    else:
+        parsing_success = False
+
+def read_next_char():
+    global look_ahead, input_stream
+    try:
+        look_ahead = next(input_stream)
+    except StopIteration:
+        look_ahead = None
+
+def S():
+    global look_ahead
+    if look_ahead not in ('0', '1'):
+        return
+
+    if look_ahead == '0':
+        match2('0')
+        S()
+        if look_ahead == '1': 
+            match2('1')
+    elif look_ahead == '1':
+        match2('1')
+        S()
+        if look_ahead == '0':
+            match2('0')
+    
+def parser_1(w: str) -> bool:
+    global input_stream, parsing_success
+    input_stream = iter(w + "$")
+    parsing_success = True
+
+    read_next_char()
+    S()
+
+    if look_ahead == "$" and parsing_success:
+        return True
+    else:
+        return False
 
 
